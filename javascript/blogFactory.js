fe1b40b4d9986b2f1cf47308d1f86948a21990d9
Christopher Miller <camiller.yr@gmail.com>
2017-10-11 13:40:05 -0500

    Changes

--- javascript/blogFactory.js
+++ javascript/blogFactory.js
@@ -6,6 +6,14 @@ const blog1 = {
     "content": ["We are on day 4 of the full-stack software development bootcamp, and so far it is going well. The material can be confounding at times, but I have been assured that it will fall into place as long as I continue to forge ahead."]
 }
 
+const blog2 = {
+    "tite": "Mid week 2 update",
+    "dateOfPublication": "2017-10-11",
+    "tags": ["JSON", "Document Methods", "NSS", "Check In"],
+    "author": "Chris Miller",
+    "content": ["We are on day 8 and have been reconfiguring our personal sites to dynamically load data to our sites using JSON / localStorage."]
+}
+
 const blogDatabase = [blog1]
 
 let blogDatabaseString = JSON.stringify(blogDatabase)
--- javascript/contactFactory.js
+++ javascript/contactFactory.js
@@ -3,28 +3,28 @@ const contact1 = {
     "service": "phone",
     "handle": "Chris Miller",
     "location": "502.314.9457",
-    "icon": ""
+    "icon": "../images/phone_icon.svg"
 }
 
 const contact2 = {
     "service": "address",
     "handle": "Chris Miller",
     "location": "2700 Shadow Ln, Nashville, TN, 37216",
-    "icon": ""
+    "icon": "../images/address_icon.svg"
 }
 
 const contact3 = {
     "service": "facebook",
     "handle": "Chris Miller",
     "location": "https://www.facebook.com/chris.miller.712714",
-    "icon": "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-1/c34.0.133.133/1890981_10100127174882519_395666419_n.jpg?oh=f0f296370d3e3230a5aa1f95ceff952e&oe=5A80987C"
+    "icon": "../images/facebook_icon.svg"
 }
 
 const contact4 = {
     "service": "twitter",
     "handle": "camiller_yr",
     "location": "https://twitter.com/camiller_yr",
-    "icon": ""
+    "icon": "../images/twitter_icon.svg"
 }
 
 const contactDatabase = [contact1, contact2, contact4, contact4]
